c8f20b1983bd371d8e36d4c7572f12f691d890ce
Philip Rosen <philipjrosen@gmail.com>
2017-08-10 19:54:12 -0700

    implement test youtube api call

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import API_KEY from '../api_keys';
 import SearchBar from './components/search_bar';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (

8d1a477945ec72bdb17966fe5c6ca8833e4623b9
Philip Rosen <philipjrosen@gmail.com>
2017-08-10 20:04:10 -0700

    refactor App to class component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import API_KEY from '../api_keys';
 import SearchBar from './components/search_bar';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));

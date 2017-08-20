449b2161afbc030c4861fcafa003412ef324c424
Philip Rosen <philipjrosen@gmail.com>
2017-08-20 16:40:18 -0700

    add ability to search for new videos

--- src/index.js
+++ src/index.js
@@ -9,12 +9,17 @@ import VideoDetail from './components/video_detail';
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       videos: [],
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -25,7 +30,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
@@ -36,3 +41,4 @@ class App extends Component {
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
+
